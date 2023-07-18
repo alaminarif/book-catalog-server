dfda91b3817833fd0d0181a041cf6ca727b82285
al-amin <arifurr231@gmail.com>
2023-07-18 10:43:38 +0600

    book done

--- src/app/module/user/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("./user.controller");
-
-router.get("/:email", UserController.getUser);
-router.post("/", UserController.createUser);
-
-module.exports = router;
